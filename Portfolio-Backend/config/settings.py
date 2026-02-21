efc5cb44d675ed0829e44f8bb7d366b7206aeb49
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-21 20:52:12 +0530

    add netlify

--- Portfolio-Backend/config/settings.py
+++ Portfolio-Backend/config/settings.py
@@ -33,10 +33,15 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://my-portfolio-1-8zu9.onrender.com"
+    # "https://my-portfolio-1-8zu9.onrender.com"
+    "https://lakshit-poojari-portfolio.netlify.app"
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True   # temporary test
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://lakshit-poojari-portfolio.netlify.app",
+]
 # Application definition
 
 INSTALLED_APPS = [
