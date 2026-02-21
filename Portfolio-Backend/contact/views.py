12e63a70a7edb200e46bfa13a995969c72f55521
Lakshit Poojari <lakshitp.2110@gmail.com>
2026-02-21 21:10:02 +0530

    parser

--- Portfolio-Backend/contact/views.py
+++ Portfolio-Backend/contact/views.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.core.mail import send_mail
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
 @api_view(['POST'])
+@parser_classes([JSONParser])
 def contact(request):
     name = request.data.get("name")
     phone = request.data.get("phone")
